ebe9a1ccbead89c66cad65dd1ee32831ab26f2a0
yaito3014 <ykakeyama3014@gmail.com>
2025-06-03 10:43:06 +0900

    split test case

--- tests/parse.cpp
+++ tests/parse.cpp
@@ -11,6 +11,10 @@ BOOST_AUTO_TEST_CASE(null)
     const auto x = json::parse("null");
     BOOST_ASSERT((x.get_kind() == yk::json20::json_value_kind::null));
   }
+}
+
+BOOST_AUTO_TEST_CASE(boolean)
+{
   {
     using json = yk::json20::basic_json<char>;
     const auto x = json::parse("true");
